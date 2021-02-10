be037ab97acb9327d69c3c4455da515fb4e9218b
jameals <jameal.samhouri@noaa.gov>
2021-02-10 11:56:54 -0800

    checking out astoria issue with DTS

--- scripts/inspect_tix.R
+++ scripts/inspect_tix.R
@@ -21,3 +21,26 @@ tix_2019 %>%
     tot_rev = sum(revenue),
     tot_adj_rev = sum(adj_revenue)
   )
+
+# weird albacore trip
+tix_2019 %>%
+  filter(
+    trip_id == 605944705
+  )
+
+tix_2019[which(tix_2019$trip_id == 605944705),]$revenue
+
+# check out Astoria, based on email from Dan Holland on 2/9/21
+tix_2019 %>%
+  filter(
+    IOPAC == "Astoria"
+  ) %>%
+  filter(
+    SPGRPN2 == 1 # DTS
+  ) %>%
+  summarise(
+    tot_rev = sum(revenue),
+    tot_adj_rev = sum(adj_revenue),
+    num_vessels = length(unique(drvid))
+  )
+
