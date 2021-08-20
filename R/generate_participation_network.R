1e7826fcae0b7b3188097ec94bcdc624a571927a
mfisher5 <mfisher5@uw.edu>
2021-08-19 17:23:38 -0700

    modify fx and generate networks with edge as proportion of vessels

--- R/generate_participation_network.R
+++ R/generate_participation_network.R
@@ -10,13 +10,17 @@
 #' @param filter use the `min_vessels` and `min_contribution` objects to filter the data
 #' @param filter_subgraph a filtering option from the original function that was turned off for Fisher et al.
 #' @param min_vessels the minimum number of vessels participating in a fishery for that fishery to be retained in the network
-#' @param min_contribution the minimum contribution (as a proportion) to total exvessel revenue for a fishery to be retained for a given vessel
+#' @param min_contribution the minimum contribution (as a proportion) to total exvessel revenue across all vessels for a fishery to be retained in the network
+#' @param min_rev the minimum revenue (in dollars) generated from all fisheries for a given vessel in a given year
+#' @param min_rev_indiv the minimum revenue (in dollars) generated from any one fishery for a given vessel in a given year
 #' @param write_out specificy whether to write out the adjacency matrix A that is used to build the graph. not yet coded in below, will make it easier if you want to manually adjust the igraph vertex / edge attributes later. 
 #' @return non-confidential fisheries partition network as an igraph object
 #' @examples
 #' close_g <- participation_network_crabyear(close_dat, filter = TRUE, filter_subgraph = FALSE)
 #' @export
-participation_network_crabyear <- function(tickets, edge_type="connectivity", pcid_choose=NA, year_choose=NA, filter, filter_subgraph, min_vessels = 3, min_contribution = 0.10, write_out, out_dir){
+
+participation_network_crabyear <- function(tickets, edge_type="connectivity", pcid_choose=NA, year_choose=NA, filter, filter_subgraph, min_vessels = 3, min_contribution = 0.10, min_rev = 1, min_rev_indiv = 1, write_out, out_dir){
+
   if(!is.na(pcid_choose)){
     tickets = dplyr::filter(tickets, IOPAC %in% pcid_choose) # updated 03-01-21, was pcgroup %in% pcid_choose
   }
@@ -41,22 +45,54 @@ participation_network_crabyear <- function(tickets, edge_type="connectivity", pc
   boats <- tickets %>% filter(drvid != 'NONE') %>%
     group_by(drvid, SPGRPN2, crab_year) %>% #removed mutate; changed year to crab_year MF 2/26/2019
     summarise(revenue = sum(adj_revenue)) %>% #changed summarize to summarise, JS 11092018
-    spread(SPGRPN2, revenue, fill = NA)
+    pivot_wider(names_from=SPGRPN2, values_from=revenue, values_fill = NA)
   boats <- as.data.frame(boats)
   rownames(boats) <- paste(boats$drvid, boats$crab_year, sep="_")
   boats$drvid <- NULL
   boats$crab_year <- NULL
   
-  # remove the one boat that didn't sell catch (i.e. rev = 0)
-  if(any(rowSums(boats,na.rm=T)==0)){boats <- boats[-which(rowSums(boats, na.rm=T)==0),]}
+  # remove the one boat that didn't sell catch (i.e. rev = 0). removed 08132021
+  # if(any(rowSums(boats,na.rm=T)==0)){boats <- boats[-which(rowSums(boats, na.rm=T)==0),]}
   
-  # remove boats that don't generate at least $5000 in revenue annually. added 08112021
-  if(any(rowSums(boats,na.rm=T)<5000)){boats <- boats[-which(rowSums(boats, na.rm=T)<5000),]}
+  # remove boats that don't generate at least min_rev in revenue annually. added 08112021
+  if(any(rowSums(boats,na.rm=T)<min_rev)){boats <- boats[-which(rowSums(boats, na.rm=T)<min_rev),]}
   
+  ##### calculate percent contributions, then remove fisheries with below min_rev_indiv #####
   # make a new df with annual % revenue from each metier for each boat
   percent_boats <- boats/rowSums(boats, na.rm = T)
+  percent_boats_mat <- as.matrix(percent_boats)
+  # for each boat, set percent_boats table value as "NA" for fisheries that don't generate at least min_rev_indiv in revenue annually. added 08192021
+  percent_boats_mat[which(boats<min_rev_indiv)] <- NA
+  percent_boats <- as.data.frame(percent_boats_mat, row.names=rownames(percent_boats_mat), col.names=colnames(percent_boats_mat))
+  rm(percent_boats_mat)
+  # for each boat, set filtered_boats table value as "NA" for fisheries that don't generate at least min_rev_indiv in revenue annually.
+  boats_mat <- as.matrix(boats)
+  boats_mat[which(boats_mat<min_rev_indiv)] <- NA
+  filtered_boats <- as.data.frame(boats_mat, row.names=rownames(boats_mat), col.names=colnames(boats_mat))
+
   
-  # find median contribution of fisheries across vessels
+  ##### OR remove fisheries first, then calculate percent contribution ##### -- hashed out by MF 08192021
+  # for each boat, set table value as "NA" for fisheries that don't generate at least min_rev_indiv in revenue annually. added 08192021
+  # if(any(boats < min_rev_indiv, na.rm=TRUE)){boats[which(boats < min_rev_indiv),] <- NA}
+  
+  #!! MF -- the above didn't work for me; I still have values < 500 in the matrix. I had to use the following code
+  # boats_mat <- as.matrix(boats)
+  # boats_mat[which(boats_mat<min_rev_indiv)] <- NA
+  # boats <- as.data.frame(boats_mat, row.names=rownames(boats_mat), col.names=colnames(boats_mat))
+  
+  # boats <- boats %>%
+  #   mutate(
+  #     revenue = case_when(
+  #     revenue < min_rev_indiv ~ NA,
+  #     revenue == revenue
+  #     )
+  #   )
+  
+  # make a new df with annual % revenue from each metier for each boat
+  # percent_boats <- boats/rowSums(boats, na.rm = T)
+  
+  
+  # find median contribution of fisheries across vessels (with min_rev_indiv applied)
   percent_contribution = apply(percent_boats, MARGIN = 2, FUN = function(x) median(x, na.rm=T))
   
   # process data: drop metiers if fewer than 3 boats participate
@@ -69,9 +105,12 @@ participation_network_crabyear <- function(tickets, edge_type="connectivity", pc
     percent = 0
   }
   
+  # make a new df with median contribution of fisheries across vessels
   fishery_df = as.data.frame(percent_contribution)
   fishery_df$SPGRPN2 = rownames(fishery_df)
   rownames(fishery_df) <- NULL
+  
+  # make a new df with vessels per fishery
   fish_df <- left_join(fishery_df, n_boats, by = 'SPGRPN2')
   
   fisheries <- fish_df$SPGRPN2[which(fish_df$max_boats>= nb & 
@@ -86,6 +125,9 @@ participation_network_crabyear <- function(tickets, edge_type="connectivity", pc
   if(length(fisheries)==0){
     return(NA)
   }
+  
+  # get total number of boats INCLUDED IN THE NETWORK - added MF 20210819
+  network_fleet_size <- dim(percent_boats)[1]
   
   # build adjacency matrix
   A <- matrix(ncol = length(fisheries), nrow = length(fisheries), data = 0)
@@ -121,13 +163,24 @@ participation_network_crabyear <- function(tickets, edge_type="connectivity", pc
         }
       }
       #if(k %% 1000 == 0){cat(paste(' iteration', k))}
-    } #end "vessels"
+    } 
+    A_counts <- A
+    # matrix where elements are proportion of vessels participating in fishery i & j (out of total # vessels included in network)
+    A_confidential <- A_counts / network_fleet_size
+    # non-confidential version of matrix A
+    A <- A_confidential; A[which(A_counts < 3)] <- 0
     
   } else{stop("ERROR: unrecognized edge type; cannot generate input matrix.")}
   
   
   if(write_out){
-    write.csv(A, here::here(out_dir,paste0("A_",pcid_choose, "_", year_choose,".csv")), row.names=FALSE)
+    if(edge_type=="connectivity"){
+      write.csv(A, here::here(out_dir,paste0("A_",pcid_choose, "_", year_choose,".csv")), row.names=FALSE)
+    } else if(edge_type=="vessels"){
+      # write out both confidential and non-confidential versions of the A matrix.
+      write.csv(A, here::here(out_dir,paste0("A_",pcid_choose, "_", year_choose,".csv")), row.names=TRUE)
+      write.csv(A_confidential, here::here(out_dir,paste0("A_",pcid_choose, "_", year_choose,"_confidential.csv")), row.names=TRUE)
+    }
   }
   
   # create graph
@@ -135,13 +188,14 @@ participation_network_crabyear <- function(tickets, edge_type="connectivity", pc
   vertex_size = sum(boats[,fisheries], na.rm=T)
   V(g)$size <- vertex_size #total revenue in all fisheries
   V(g)$percent_size = apply(percent_boats[,fisheries, drop=FALSE], MARGIN = 2, FUN = function(x) median(x, na.rm=T))
-  V(g)$importance = V(g)$size*V(g)$percent_size #how much revenue from each fishery
+  V(g)$importance = V(g)$size*V(g)$percent_size #how much revenue from each fishery. note that the plot_networks.R function relies on the igraph library, which scales all nodes to the max size. so in practice, node size is based simply on percent_size and all node sizes are rellattive within a networ
   
-  V(g)$fleet = fleet_size #total number of vessels in fishery (MF2/26/2019)
+  V(g)$fleet = fleet_size #total number of vessels active in given port group, fishing year (MF2/26/2019)
   
   vessel_df2 <- left_join(data.frame(v=V(g)$name),vessel_df, by = "v") # added JS 01/27/2021
   V(g)$vessels <- vessel_df2$n # added JS 01/27/2021
   #V(g)$vessels = vessels # added JS 01/26/2021, replaced with 2 lines above
+  V(g)$total_vessels <- network_fleet_size #total number of vessels in network (differs from "fleet" attribute assigned on line ) - added MF 20210819
   
   ##########  changed 2/26/2019 ############
   # if filter_subgraph = TRUE, keep V which make up to 99% of revenue
